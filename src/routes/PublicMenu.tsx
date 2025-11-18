cfe8ebd2166b2b3c233ccdf91572d88e21cb12ce
Andrew Rowley <andrew@internetdrew.com>
2025-11-18 14:57:45 -0500

    switch back to fixed

--- src/routes/PublicMenu.tsx
+++ src/routes/PublicMenu.tsx
@@ -64,7 +64,7 @@ const PublicMenu = () => {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <nav className="bg-background mx-auto flex w-full max-w-screen-sm items-center justify-between px-4 py-4">
+      <nav className="bg-background fixed top-0 right-0 left-0 mx-auto flex w-full max-w-screen-sm items-center justify-between px-4 py-4">
         <h1 className="text-lg font-semibold">{menu.place.name}</h1>
         <DropdownMenu>
           <DropdownMenuTrigger aria-label="Menu Categories">
