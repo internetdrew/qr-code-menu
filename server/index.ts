b8d07fba4758c82b21897a1244f009c507f879bf
Andrew Rowley <andrew@internetdrew.com>
2025-11-28 12:50:49 -0500

    add stripe endpoint and rewrite

--- server/index.ts
+++ server/index.ts
@@ -34,6 +34,7 @@ const app = express();
 app.use(compression());
 app.use(helmet());
 app.use(cors(corsOptions));
+app.use("/api/stripe/webhook", express.raw({ type: "application/json" }));
 app.use(express.json());
 app.use(cookieParser());
 
