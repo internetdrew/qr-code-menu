2679c687341f5e8dac5e750d61f3d12018a22d51
Andrew Rowley <andrew@internetdrew.com>
2025-12-17 11:56:56 -0500

    switch to user feedback semantics

---
+++ server/routers/userFeedbackRouter.ts
@@ -0,0 +1,35 @@
+import z from "zod";
+import { TRPCError } from "@trpc/server";
+import { protectedProcedure, router } from "../trpc";
+import { supabaseAdminClient } from "../supabase";
+
+export const userFeedbackRouter = router({
+  submit: protectedProcedure
+    .input(
+      z.object({
+        feedback: z.string().min(1).max(500),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { feedback } = input;
+
+      const { data: newFeedback, error: feedbackError } =
+        await supabaseAdminClient
+          .from("user_feedback")
+          .insert({
+            user_id: ctx.user.id,
+            feedback,
+          })
+          .select()
+          .single();
+
+      if (feedbackError) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Failed to submit feedback: ${feedbackError.message}`,
+        });
+      }
+
+      return newFeedback;
+    }),
+});
