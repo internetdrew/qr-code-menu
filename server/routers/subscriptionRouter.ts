7a37947c246d2963f73c15193c38eaf939ba3137
Andrew Rowley <andrew@internetdrew.com>
2025-12-18 13:41:01 -0500

    update subs to user based

--- server/routers/subscriptionRouter.ts
+++ server/routers/subscriptionRouter.ts
@@ -1,25 +1,22 @@
-import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { protectedProcedure, router } from "../trpc";
 import { supabaseAdminClient } from "../supabase";
 
 export const subscriptionRouter = router({
-  getForPlace: protectedProcedure
-    .input(z.object({ placeId: z.string() }))
-    .query(async ({ input }) => {
-      const { data: place, error } = await supabaseAdminClient
-        .from("subscriptions")
-        .select("*")
-        .eq("place_id", input.placeId)
-        .maybeSingle();
+  getForUser: protectedProcedure.query(async ({ ctx }) => {
+    const { data: subscription, error } = await supabaseAdminClient
+      .from("subscriptions")
+      .select("*")
+      .eq("user_id", ctx.user.id)
+      .maybeSingle();
 
-      if (error) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: error.message,
-        });
-      }
+    if (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: error.message,
+      });
+    }
 
-      return place;
-    }),
+    return subscription;
+  }),
 });
