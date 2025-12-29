ddf3b073c4e138225284fb6c3b2863b5593c2196
Andrew Rowley <andrew@internetdrew.com>
2025-12-29 15:22:13 -0500

    reinstate route export

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,25 @@
+import Login from "./routes/Login.tsx";
+import { Menu } from "./routes/Menu.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import { MenuProvider } from "./contexts/ActiveMenuContext.tsx";
+import App from "./App.tsx";
+import { HomePage } from "./routes/HomePage.tsx";
+import { CategoriesPage } from "./routes/CategoriesPage.tsx";
+import { CategoryItemsPage } from "./routes/CategoryItemsPage.tsx";
+import { SettingsPage } from "./routes/SettingsPage.tsx";
+import { NotFound } from "./routes/NotFound.tsx";
 import { Navigate, Outlet } from "react-router";
-import Login from "./routes/Login";
-import { Menu } from "./routes/Menu";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { MenuProvider } from "./contexts/ActiveMenuContext";
-import App from "./App";
-import { HomePage } from "./routes/HomePage";
-import { CategoriesPage } from "./routes/CategoriesPage";
-import { CategoryItemsPage } from "./routes/CategoryItemsPage";
-import { SettingsPage } from "./routes/SettingsPage";
-import { NotFound } from "./routes/NotFound";
+import RedirectIfAuthenticated from "./components/RedirectIfAuthenticated.tsx";
 
 export const routes = [
   {
-    path: "/login",
-    element: <Login />,
+    element: <RedirectIfAuthenticated />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
   {
     path: "/preview/menu",
