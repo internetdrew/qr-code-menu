3a904fe1a60bd15845bae83e1baa96ce80529942
Andrew Rowley <andrew@internetdrew.com>
2025-12-30 09:43:00 -0500

    add warning for unhandled requests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -15,6 +15,10 @@ Object.defineProperty(window, "matchMedia", {
   }),
 });
 
-beforeAll(() => server.listen());
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest: "warn",
+  }),
+);
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
