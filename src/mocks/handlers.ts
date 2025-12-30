59d5a376161871e7175f63641172e81a71b89c1a
Andrew Rowley <andrew@internetdrew.com>
2025-12-30 09:46:28 -0500

    empty handlers for now

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,1 @@
-import { http, HttpResponse } from "msw";
-
-export const handlers = [
-  http.get("https://api.example.com/user", () => {
-    return HttpResponse.json({
-      id: "abc-123",
-      firstName: "John",
-      lastName: "Maverick",
-    });
-  }),
-];
+export const handlers = [];
